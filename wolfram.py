58f38fc6303b832cd45718b89186a6086351a2c0
varunrau <varunrau@gmail.com>
2013-04-19 13:37:16 -0700

    removed unnecessary import

--- wolfram.py
+++ wolfram.py
@@ -1,10 +1,9 @@
 import sys
 import urllib2
-import urllib
 from xml.dom.minidom import parseString
 
 query = " ".join(sys.argv[1:])
-file = urllib2.urlopen('http://api.wolframalpha.com/v2/query?input=' + urllib.quote(query, "") + '&appid=695RVU-8V9WUKATK2')
+file = urllib2.urlopen('http://api.wolframalpha.com/v2/query?input=' + urllib2.quote(query, "") + '&appid=695RVU-8V9WUKATK2')
 data = file.read()
 dom = parseString(data)
 xmlTag = dom.getElementsByTagName("plaintext")[1].toxml()
